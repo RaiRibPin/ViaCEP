1aaff940e2f56f259bc509099f9c3ccd37cf96c2
RaiRibPin <raissarp470717@gmail.com>
2025-03-10 20:09:54 -0300

    Objeto console

--- App.js
+++ App.js
@@ -4,22 +4,37 @@ import { PaperProvider, TextInput, Text, Button } from 'react-native-paper';
 
 export default function App() {
     let [cep, setCep] = React.useState("");
+    let [render, setRender] = React.useState([]);
     
     const BuscaCep = (xcep) => {
         let url = `https://viacep.com.br/ws/${xcep}/json/`;
         console.log(url);
+
+        fetch(url)
+            .then(
+                (resp) => { return resp.json() }
+            ).then(
+                (dados)=>{
+                    console.log(dados);
+
+                    console.log(dados.logradouro);
+                    console.log(dados['logradouro']);
+                    setRender(dados);
+
+                }
+            ).catch(
+                (erro) => {console.log(erro)}
+            )
     }
 
     return (
             <PaperProvider>
                 <View style = {{ marginTop: 50, marginHorizontal:20}}>
-                    <Text style={{color:'#000'}}>ViaCEP</Text> 
-
-                    <TextInput label={'CEP:'} mode='outlined' onChangeText={(value)=>{setCep(value)}} />
-
+                    <Text style={{color:'#A7C'}}>ViaCEP</Text> 
+                    <TextInput style={{backgroundColor:'#FFF'}} label={'CEP:'} mode='outlined' onChangeText={(value)=>{setCep(value)}} />
                     <Button icon={"tab-search"} onPress={()=>{BuscaCep(cep)}} 
                     mode='contained' style={{marginTop:20}}>Busca</Button>
                 </View>
-            </PaperProvider>
+            </PaperProvider>    
     );
-};
+};  
